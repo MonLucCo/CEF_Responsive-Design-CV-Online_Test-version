321b9fc77885373b57ae2d44cd666274524cf646
luc.perard@hotmail.fr <116542956+MonLucCo@users.noreply.github.com>
2024-11-19 21:56:46 +0100

    Etape6__Réalisation du Footer et gestion des événément (Scroll Top). Correction du Header pour harmonisation de rendu et gestion des événements dans App. Modification de App pour harmonisation dans Main (Composant fonctionnel) des événéments

--- cv-online/src/App.js
+++ cv-online/src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Home from './pages/Home';
 import Services from './pages/Services';
 import Projects from './pages/Projects';
@@ -14,8 +14,69 @@ import './assets/styles/App.css';
 const App = () => {
   return (
     <Router>
+      <Main />
+    </Router>
+  );
+}
+
+const Main = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    const scrollTopButton = document.querySelector('.scroll-top');
+    const mainContent = document.querySelector('.main-content');
+    const header = document.querySelector('header');
+    const navbarToggler = document.querySelector('.navbar-toggler');
+    const navLinks = document.querySelectorAll('.nav-link');
+
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        scrollTopButton.classList.remove('d-none');
+      } else {
+        scrollTopButton.classList.add('d-none');
+      }
+    };
+
+    const setPaddingTop = () => {
+      const headerHeight = header.offsetHeight;
+      mainContent.style.paddingTop = `${headerHeight}px`;
+    };
+
+    const closeNavbar = (event) => {
+      const navbarCollapse = document.querySelector('.navbar-collapse');
+      const targetPath = event.currentTarget.getAttribute('href');
+      if (navbarCollapse.classList.contains('show')) {
+        navbarToggler.click();
+      }
+      if (location.pathname !== targetPath) {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+      }
+    };
+
+    document.addEventListener('scroll', handleScroll);
+    window.addEventListener('resize', setPaddingTop);
+
+    navLinks.forEach(link => {
+      link.addEventListener('click', closeNavbar);
+    });
+
+    setPaddingTop();
+
+    scrollTopButton.addEventListener('click', () => {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
+
+    return () => {
+      document.removeEventListener('scroll', handleScroll);
+      window.removeEventListener('resize', setPaddingTop);
+      navLinks.forEach(link => { link.removeEventListener('click', closeNavbar); });
+    };
+  }, []);
+
+  return (
+    <>
       <Header />
-      <main className="container">
+      <main className="main-content">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/services" element={<Services />} />
@@ -27,7 +88,7 @@ const App = () => {
         </Routes>
       </main>
       <Footer />
-    </Router>
+    </>
   );
 };
 
