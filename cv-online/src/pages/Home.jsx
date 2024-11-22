cb29fbb16ada5c08834ab52266ce2d6e22dc0c43
luc.perard@hotmail.fr <116542956+MonLucCo@users.noreply.github.com>
2024-11-22 20:16:50 +0100

    Etape9_Correction des bugs et refactoring pour les fichiers Sass

--- cv-online/src/pages/Home.jsx
+++ cv-online/src/pages/Home.jsx
@@ -19,53 +19,37 @@ const Home = () => {
         { name: 'React', level: '50%', color: 'bg-primary' }, // $bleu-400
     ];
 
-    /// Fonction pour calculer l'offset
-    const calculateOffset = () => {
-        const offsetValue = getComputedStyle(document.documentElement).getPropertyValue('--offset-scroll');
-        const offsetUnit = offsetValue.match(/[a-zA-Z]+/)[0]; // Extraire l'unité (rem)
-        const offsetNumber = parseFloat(offsetValue); // Extraire la valeur numérique
-        let offset = 0;
-        if (offsetUnit === 'rem') {
-            const rootFontSize = parseFloat(getComputedStyle(document.documentElement).fontSize);
-            offset = offsetNumber * rootFontSize; // Convertir rem en pixels
-        } else {
-            offset = offsetNumber; // Si l'unité est déjà en pixels
-        } return offset;
-    };
-
-    // Fonction pour faire défiler la page jusqu'à la section "À propos" avec un décalage
-    const scrollToAbout = () => {
+    // Fonction pour faire défiler la page jusqu'à la section "À propos" avec recalage des Top-offsets
+    function scrollToAbout() {
         const aboutSection = document.getElementById('about');
-        const offset = calculateOffset(); // Utiliser la fonction pour calculer l'offset
-        console.log('Offset value:', offset); // Afficher la valeur de offset dans la console
-        const bodyRect = document.body.getBoundingClientRect().top;
-        const elementRect = aboutSection.getBoundingClientRect().top;
-        const elementPosition = elementRect - bodyRect;
-        const offsetPosition = elementPosition - offset;
-        window.scrollTo({
-            top: offsetPosition,
-            behavior: 'smooth'
-        });
-    };
+        const aboutOffsetTop = parseFloat(window.getComputedStyle(aboutSection).marginTop)
+        const homeSection = document.getElementById('home');
+        const homeOffsetTop = parseFloat(window.getComputedStyle(homeSection).marginTop)
+        if (aboutSection) {
+            window.scrollTo({
+                top: aboutSection.offsetTop - aboutOffsetTop - homeOffsetTop,
+                behavior: 'smooth'
+            });
+        }
+    }
+
 
     return (
-        <div className="home">
-            <div className="hero-section d-flex flex-column justify-content-center align-items-center">
-                <div className="overlay"></div>
-                <div className="container text-center">
+        <div id='home' className="home-page">
+            <section className="hero-section d-flex flex-column justify-content-center align-items-center">
+                <div className="overlay z-1"></div>
+                <div className="container z-2 text-center">
                     <h1>Bonjour, je suis John Doe</h1>
                     <h2>Développeur web full stack</h2>
-                    <button onClick={scrollToAbout} className="btn btn-primary mt-3">
-                        En savoir plus
-                    </button>
+                    <button onClick={scrollToAbout} className="btn btn-primary mt-3"> En savoir plus </button>
                 </div>
-            </div>
-            <section id="about" className="about-section py-5">
+            </section>
+            <section id="about" className="about-section m-5">
                 <div className="container">
                     <div className="card shadow"> {/* Ajout de la classe shadow pour l'ombre */}
                         <div className="card-body p-5">
                             <div className="row">
-                                <div className="col-12 col-md-6 col-lg-6">
+                                <div className="col-12 col-md-6">
                                     <h2 className="text-start">À propos</h2>
                                     <hr className="mt-2 mb-4" /> {/* Barre de séparation pour la première colonne */}
                                     <p>
@@ -83,7 +67,7 @@ const Home = () => {
                                         respecte les bonnes pratiques du web.
                                     </p>
                                 </div>
-                                <div className="col-12 col-md-6 col-lg-6 text-center">
+                                <div className="col-12 col-md-6">
                                     <img
                                         src={johnDoeAbout}
                                         alt="John Doe"
